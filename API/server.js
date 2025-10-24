fc3829bad44f2068c767f96c0d0c7111ef6d9660
Mateo <mateas2014@gmail.com>
2025-10-24 18:16:26 -0300

    vercel.json

--- API/server.js
+++ API/server.js
@@ -1,23 +1,10 @@
 import express from 'express';
 import TodasLasRutas from './routes/index.js';
 import cors from 'cors';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-
-// Servir archivos estáticos desde la raíz
-app.use(express.static(__dirname));
-
-// Ruta raíz -> devuelve index.html
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'index.html'));
-});
 
 // Tus rutas de la API
 TodasLasRutas(app);
